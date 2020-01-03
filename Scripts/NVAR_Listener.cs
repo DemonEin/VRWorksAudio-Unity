c76ba544be26bf6f44e61c70ffd068b5e5d15cd7
DemonEin <danielwarner00@gmail.com>
2020-01-03 13:55:26 -0800

    added additional base files

---
+++ Scripts/NVAR_Listener.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.VRWorksAudio.Internal;
+
+[RequireComponent(typeof(AudioSource))]
+public class NVAR_Listener : MonoBehaviour {
+    private AudioSource output;
+    internal NVAR.Context context;
+    private NVAR.Material material;
+    //private AudioClip nextClip;
+    public void SetData (float[][] data)
+    {
+        float[] monoData = new float[data[0].Length * 2];
+        Debug.Log(data[0].Length);
+        Debug.Log(monoData.Length);
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            monoData[i * 2] = data[0][i];
+            monoData[i * 2 + 1] = data[1][i];
+        }
+        output.clip.SetData(monoData, 0);
+        output.Play();
+    }
+	// Use this for initialization
+	void Awake () {
+        output = GetComponent<AudioSource>();
+        //NVAR operations
+        NVAR.Initialize(0);
+        NVAR.Create(out context, "test", NVAR.EffectPreset.Low);
+        NVAR.CreateMaterial(context, out material);
+
+        output.clip = AudioClip.Create("test", 220500, 2, 44100, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("up"))
+        { 
+            NVAR.TraceAudio(context);
+        }
+        NVAR.SetListenerLocation(context, gameObject.transform.position);
+        NVAR.SetListenerOrientation(context, gameObject.transform.forward, gameObject.transform.up);
+    }
+}
---
+++ Scripts/NVAR_Source.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.VRWorksAudio.Internal;
+
+[RequireComponent(typeof(AudioSource))]
+public class NVAR_Source : MonoBehaviour {
+    private AudioSource audioSource;
+    private NVAR.Source NVARsource;
+    private NVAR_Listener listener; 
+    private float[][] audioDataOut;
+    private float[] audioDataIn;
+    // Use this for initialization
+    void Start () {
+        listener = Camera.main.gameObject.GetComponent<NVAR_Listener>();
+        audioSource = GetComponent<AudioSource>();
+        audioDataIn = new float[audioSource.clip.samples];
+        Debug.Log(listener.context.isNull);
+        Debug.Log(NVAR.CreateSource(listener.context, NVAR.EffectPreset.Medium, out NVARsource));
+        audioSource.clip.GetData(audioDataIn, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("down"))
+        {
+            sendAudio();
+        }
+        NVAR.SetSourceLocation(NVARsource, gameObject.transform.position);
+    }
+    private void sendAudio ()
+    {
+        Debug.Log(NVAR.ApplySourceFilters(NVARsource, out audioDataOut, audioDataIn, audioSource.clip.samples));
+        NVAR.ApplySourceFilters(NVARsource, out audioDataOut, audioDataIn, audioSource.clip.samples);
+        listener.SetData(audioDataOut);
+    }
+}
