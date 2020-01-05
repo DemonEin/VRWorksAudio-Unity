2e819a71391b4957b44aa2c38bc02c6df0215838
DemonEin <danielwarner00@gmail.com>
2020-01-05 11:47:04 -0800

    cleaned old code

--- Scripts/NVAR_Listener.cs
+++ Scripts/NVAR_Listener.cs
@@ -2,36 +2,19 @@
 using NVIDIA.VRWorksAudio.Internal;
 using System.Threading;
 
-[RequireComponent(typeof(AudioSource))]
 public class NVAR_Listener : MonoBehaviour {
     //used to determine the buffer length of filtered audio; if the Framerate goes below minFramerate, the filters will not be applied in time for proper playback; higher improves performance
     public readonly float minFramerate = 10;
-    private AudioSource output;
     internal NVAR.Context context;
+
     private EventWaitHandle traceWaitHandle;
-    //private AudioClip nextClip;
-    public void SetData (float[][] data)
-    {
-        float[] monoData = new float[data[0].Length * 2];
-        Debug.Log(data[0].Length);
-        Debug.Log(monoData.Length);
-        for (int i = 0; i < data[0].Length; i++)
-        {
-            monoData[i * 2] = data[0][i];
-            monoData[i * 2 + 1] = data[1][i];
-        }
-        output.clip.SetData(monoData, 0);
-        output.Play();
-    }
+
 	// Use this for initialization
 	void Awake () {
         traceWaitHandle = new EventWaitHandle(true, EventResetMode.AutoReset);
-        output = GetComponent<AudioSource>();
         NVAR.Initialize(0);
-        NVAR.Create(out context, "test", NVAR.EffectPreset.Low);
-        output.clip = AudioClip.Create("test", 220500, 2, 44100, false);
+        NVAR.Create(out context, "VRWA-Unity", NVAR.EffectPreset.Low);
     }
-	
 	// Update is called once per frame
 	void Update () {
         NVAR.SetListenerLocation(context, gameObject.transform.position);
@@ -40,5 +23,10 @@ public class NVAR_Listener : MonoBehaviour {
         {
             NVAR.TraceAudio(context, traceWaitHandle.SafeWaitHandle.DangerousGetHandle());
         }
+    }
+    private void OnDisable()
+    {
+        NVAR.Destroy(context);
+        NVAR.Finalize();
     }
 }
--- Scripts/NVAR_Source.cs
+++ Scripts/NVAR_Source.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using NVIDIA.VRWorksAudio.Internal;
 
 [RequireComponent(typeof(AudioSource))]
@@ -9,45 +7,17 @@ public class NVAR_Source : MonoBehaviour {
     private NVAR.Source NVARsource;
     private NVAR_Listener listener;
     private AudioClip baseClip;
-    private float[][] audioDataOut;
-    private float[] audioDataIn;
-    // Use this for initialization
-    void Start () {
-        listener = Camera.main.gameObject.GetComponent<NVAR_Listener>();
-        audioSource = GetComponent<AudioSource>();
-        baseClip = audioSource.clip;
-        audioDataIn = new float[audioSource.clip.samples];
-        NVAR.CreateSource(listener.context, NVAR.EffectPreset.Medium, out NVARsource);
-        audioSource.clip = AudioClip.Create("NVARAudio", baseClip.samples, 2, 44100, false);
-        audioSource.Play();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("down"))
-        {
-            ApplyFilter();
-        }
-        ApplyFilter();
-        NVAR.SetSourceLocation(NVARsource, gameObject.transform.position);
-    }
-    private void sendAudio ()
-    {
-        Debug.Log(NVAR.ApplySourceFilters(NVARsource, out audioDataOut, audioDataIn, audioSource.clip.samples));
-        NVAR.ApplySourceFilters(NVARsource, out audioDataOut, audioDataIn, audioSource.clip.samples);
-        listener.SetData(audioDataOut);
-    }
+
     private void ApplyFilter()
     {
-        int bufferLength = (int) ((1 / listener.minFramerate) * baseClip.frequency);
+        int bufferLength = (int)((1 / listener.minFramerate) * baseClip.frequency);
         float[] audioDataIn = new float[bufferLength];
         float[][] audioDataOut;
         baseClip.GetData(audioDataIn, audioSource.timeSamples);
         NVAR.ApplySourceFilters(NVARsource, out audioDataOut, audioDataIn, audioDataIn.Length);
         audioSource.clip.SetData(ParallelToWeaved(audioDataOut), audioSource.timeSamples);
-        //audioSource.clip.SetData(audioDataIn, 0);
     }
-    private float[] ParallelToWeaved (float[][] parallel)
+    private float[] ParallelToWeaved(float[][] parallel)
     {
         float[] weaved = new float[parallel.Length * parallel[0].Length];
         int i = 0;
@@ -59,7 +29,20 @@ public class NVAR_Source : MonoBehaviour {
                 i++;
             }
         }
-        //Debug.Log(weaved.Length);
         return weaved;
+    }
+    // Use this for initialization
+    void Start () {
+        listener = Camera.main.gameObject.GetComponent<NVAR_Listener>();
+        audioSource = GetComponent<AudioSource>();
+        baseClip = audioSource.clip;
+        NVAR.CreateSource(listener.context, NVAR.EffectPreset.Medium, out NVARsource);
+        audioSource.clip = AudioClip.Create("NVARAudio", baseClip.samples, 2, baseClip.frequency, false);
+        audioSource.Play();
+    }
+	// Update is called once per frame
+	void Update () {
+        ApplyFilter();
+        NVAR.SetSourceLocation(NVARsource, gameObject.transform.position);
     }
 }
