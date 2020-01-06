36e7694dc35b39d4dcf09683fbd7588ee0f686c0
DemonEin <danielwarner00@gmail.com>
2020-01-06 11:22:15 -0800

    added basic static geometry

--- Scripts/NVAR_Geometry.cs
+++ Scripts/NVAR_Geometry.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using NVIDIA.VRWorksAudio.Internal;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class NVAR_Geometry : MonoBehaviour {
+    private NVAR_Listener listener;
+    private NVAR.Mesh NVAR_mesh;
+    private Mesh mesh;
 
+    internal NVAR.Material material;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        listener = Camera.main.gameObject.GetComponent<NVAR_Listener>();
+        mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        NVAR.CreateMaterial(listener.context, out material);
+        NVAR.CreateMesh(listener.context, out NVAR_mesh, gameObject.transform.localToWorldMatrix, mesh.vertices, mesh.triangles, mesh.triangles.Length / 3, material);
+    }
 }
